ba5d5b5fc31ba08c4c865cf1f06386114e3ba889
Leonardo Miranda Amaral <leonardo.miranda@suse.com>
2026-02-09 12:53:06 -0300

    feat: Include session token in AWS credentials
    Added session token to AWS credentials configuration.

--- roles/import-cluster/terraform/main.tf
+++ roles/import-cluster/terraform/main.tf
@@ -2,8 +2,9 @@ resource "rancher2_cloud_credential" "aws" {
     name = "aws-credentials"
     description = "AWS Credentials for EC2"
     amazonec2_credential_config {
-        access_key = var.aws["access_key"]
-        secret_key = var.aws["secret_key"]
+        access_key    = var.aws["access_key"]
+        secret_key    = var.aws["secret_key"]
+        session_token = var.aws["session_token"]
     }
 }
 
